bd856257a0bc25dd430408c6864022a00c7c90e7
Jamila Fonseca <73254741+jamilafonseca@users.noreply.github.com>
2020-11-26 10:52:53 -0300

    Add files via upload
    import Dictionary - Tabelas de Conhecimento - Tabelas DE-PARA para validação e correção dos dados

---
+++ imp_dic_ramo_justica.py
@@ -0,0 +1,15 @@
+
+
+
+dic_ramo_justica = {
+    'STF' :  (  1, 'STF' , 'Supremo Tribunal Federal'  ),
+    'CNJ' :  (  2, 'CNJ' , 'Conselho Nacional de Justiça'  ),
+    'STJ' :  (  3, 'STJ' , 'Superior Tribunal de Justiça'  ),
+    'TRF' :  (  4, 'TRF' , 'Justiça Federal'  ),
+    'TRT' :  (  5, 'TRT' , 'Justiça do Trabalho' ) ,
+    'TRE' :  (  6, 'TRE' , 'Justiça Eleitoral'   ),
+    'STM' :  (  7, 'STM' , 'Justiça Militar da União'  ),
+    'TJ'  :  (  8, 'TJ'  , 'Justiça Estadual'  ),
+    'TJM' :  (  9, 'TJM' , 'Justiça Militar Estadual' ) ,
+    'TST' :  (  0, 'TST' , 'Justiça do Trabalho - Trib Superior'  ),
+}
---
+++ imp_dic_tipo_processo.py
@@ -0,0 +1,32 @@
+
+
+#  onze tribunais se destacam positivamente por terem alcançado 100% 
+#  de processos eletrônicos nos dois graus de jurisdição: 
+#
+#  fonte:  CNJ em numeros 2020
+
+# 1 - Processo Meio Eletrônico
+# 2 - Processo Físico
+
+dic_tipo_processo = {
+  'TJAC'  : '1', 
+  'TJAL'  : '1', 
+  'TJAM'  : '1', 
+  'TJMS'  : '1', 
+  'TJPR'  : '1', 
+  'TJSE'  : '1', 
+  'TJTO'  : '1', 
+  'TRF4'  : '1', 
+  'TJMRS' : '1', 
+  'STM'   : '1', 
+  'TRT10' : '1', 
+  'TRT11' : '1', 
+  'TRT13' : '1', 
+  'TRT16' : '1', 
+  'TRT18' : '1', 
+  'TRT24' : '1', 
+  'TRT7'  : '1', 
+  'TRT9'  : '1', 
+} 
+
+
